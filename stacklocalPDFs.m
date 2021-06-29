c7d8b0db370383256b9ef305cca6809a7b00e4fc
Leora <85642780+lwils72@users.noreply.github.com>
2021-06-29 12:24:00 -0500

    05/29

--- stacklocalPDFs.m
+++ stacklocalPDFs.m
@@ -21,9 +21,28 @@ setenv('PATH', [getenv('PATH') ':/usr/local/bin']);
   %
     tic
     BHinfile='dats/Table_S1.csv';
-    FMinfile='dats/FM_subsets_asof_20200108.shmax';
+    FMinfile='dats/FM_subsets_asof_20210615.shmax';
     SSinfile='dats/LiPeng2017tableS3.csv';
     [B,S,L,F,Regions,P]=load_and_parse_SSBHFM(BHinfile,FMinfile,SSinfile); %make sure everything is in correct order with infiles uncommented
+
+   %
+   % This excludes SS that are not in the LA basin 
+   %
+     iLA=find(L.Y>P.R2(3) & L.Y<P.R2(4) & L.X>P.R2(1) & L.X<P.R2(2)); %list of iLA
+     L.stationlon=L.stationlon(iLA);
+     L.stationlat=L.stationlat(iLA);
+     L.station=L.station(iLA);
+     L.Nmeasurements=L.Nmeasurements(iLA);
+     L.ResultantLength=L.ResultantLength(iLA);
+     L.FastDirection=L.FastDirection(iLA);
+     L.DelayTime=L.DelayTime(iLA);
+     L.DelayTimeSTD=L.DelayTimeSTD(iLA);
+     L.X=L.X(iLA);
+     L.Y=L.Y(iLA);
+     S.L_SHmax=S.L_SHmax(iLA,:,:);
+     S.L_dSHmax=S.L_dSHmax(iLA,:,:);
+
+     stop
 
     % THESE ACTUALLY AREN"T BEING USED THAT MUCH... CAN... UM... clear these a bit?
     daz = @(x,y) (mod(y-x+90,180)-90);  % Circular Difference
@@ -98,7 +117,7 @@ setenv('PATH', [getenv('PATH') ':/usr/local/bin']);
 
     iNorthRidgeEQ=find(floor(F.t)==datenum([1994 01 17]) & F.M>6);
     plot3(F.X(iNorthRidgeEQ),F.Y(iNorthRidgeEQ),-F.z(iNorthRidgeEQ),'k*','markersize',20,'linewidth',2)
-stop
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SORT THE FM INVERSION SOLUTIONS INTO NEW ORDER TO MATCH THE CURRENT BH LIST
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -728,17 +747,19 @@ stop
   % Previously, this was calculated here.  Now, it's just being loaded from TableS1 as part of
   % load_and_parse_BHFM.m subroutine.
   %
-%     iLA=find(L.Y>P.R2(3) & L.Y<P.R2(4) & L.X>P.R2(1) & L.X<P.R2(2)); %list of iLA
-%     L.stationlon=L.stationlon(iLA);
-%     L.stationlat=L.stationlat(iLA);
-%     L.station=L.station(iLA);
-%     L.Nmeasurements=L.Nmeasurements(iLA);
-%     L.ResultantLength=L.ResultantLength(iLA);
-%     L.FastDirection=L.FastDirection(iLA);
-%     L.DelayTime=L.DelayTime(iLA);
-%     L.DelayTimeSTD=L.DelayTimeSTD(iLA);
-%     L.X=L.X(iLA);
-%     L.Y=L.Y(iLA);
+     iLA=find(L.Y>P.R2(3) & L.Y<P.R2(4) & L.X>P.R2(1) & L.X<P.R2(2)); %list of iLA
+     L.stationlon=L.stationlon(iLA);
+     L.stationlat=L.stationlat(iLA);
+     L.station=L.station(iLA);
+     L.Nmeasurements=L.Nmeasurements(iLA);
+     L.ResultantLength=L.ResultantLength(iLA);
+     L.FastDirection=L.FastDirection(iLA);
+     L.DelayTime=L.DelayTime(iLA);
+     L.DelayTimeSTD=L.DelayTimeSTD(iLA);
+     L.X=L.X(iLA);
+     L.Y=L.Y(iLA);
+     S.L_SHmax=S.L_SHmax(iLA,:,:);
+     S.L_dSHmax=S.L_dSHmax(iLA,:,:);
 %       this info excludes the area that isnt the LAbasin   
   
   %
